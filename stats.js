1f009f291a0355c37620971a9f1ebb176dbf7d57
martinrioss <martinrios.rrr@gail.com>
2022-12-21 01:31:55 -0300

    task4 terminada

--- stats.js
+++ stats.js
@@ -2,8 +2,6 @@ let table1=document.getElementById('table1');
 let table2=document.getElementById('upcomingTable');
 let table3=document.getElementById('tablePast')
 
-
-
 fetch('https://amazing-events.onrender.com/api/events')
   .then(res=>res.json())
   .then(data=>{
@@ -12,82 +10,58 @@ fetch('https://amazing-events.onrender.com/api/events')
     let upcomingEvents=events.filter(element=>currentDate<element.date);
     let pastEvents=events.filter(element=>element.date<currentDate);
     //renderTable1(events,pastEvents,table1);
-    
-    renderTable(pastEvents,table1)
+    renderTable1(pastEvents,events,table1)
     renderTable2(upcomingEvents,table2);
     renderTable3(pastEvents,table3);
   })
   .catch(error=>console.log(error));
 
-
-function assistance(dataPast){
-  console.table(dataPast)
-  let newarray=[]
-  dataPast.forEach((e)=>{
-    newarray.push({name:e.name,assistance:(100*e.assistance/e.capacity).toFixed(2)})
+function filterAssistance (eventsPast) {
+  let assistance= [...eventsPast] 
+  let arrayAssistance= [] 
+  assistance.forEach(propiedades => {
+      let percentage= (((propiedades.assistance*100) / propiedades.capacity))
+      if(percentage> 95) {
+      return arrayAssistance.push (`${propiedades.name}; ${((percentage).toFixed(2))}%`)
+      }
   })
-  return newarray.sort((a,b)=>b.assistance-a.assistance)
-  
-}
-function renderTable(data,container){
-  let assistance=assistance(data)
-
+  return (arrayAssistance) 
 }
 
+function filterMinerAssistance (eventsPast) {
+  let minorAssistance= [...eventsPast]
+  let arrayMinorAssistance= []
+  minorAssistance.forEach(menor => {
+      let percentage= (((menor.assistance*100) / menor.capacity))
+      if(percentage < 70) {
+      return arrayMinorAssistance.push (`${menor.name}; ${((percentage).toFixed(2))}%`) 
+      }
+  })
+  return (arrayMinorAssistance)
+}
 
-// function renderTable1(data,past,container){
+function capacity (listaHome) {
+  let capacityTotal= [...listaHome]
+  capacityTotal.sort((event1,event2)=>event2.capacity-event1.capacity)
+  return (capacityTotal) 
+}
 
-// let assistance=[...past]
-// assistance.sort((event1,event2)=>event1.assistance-event2.assistance);
-// let arr=assistance.map((element)=>[element.name,element.assistance])
-// console.log(arr)
-// let capacity=[...data]
-// capacity.sort((event1,event2)=>event2.capacity-event1.capacity)
-
-// // container.innerHTML=`
-// //   <tr>
-// //     <td>${assistance[assistance.length-1].name}</td>
-// //     <td>${assistance[0].name}</td>
-// //     <td>${capacity[0].name}</td>
-// //   </tr>
-// //   `
-// }
-// function renderPercentage (listaHome, eventsPast,containerHome) {
-//   let assistance= [...eventsPast]
-//   let arrayAssistance= [] 
-//   console.log(arrayAssistance)
-//   assistance.forEach(propiedades => {
-//       let percentage= (((propiedades.assistance100) / propiedades.capacity))
-//       if(percentage> 95) {
-//       return arrayAssistance.push (`${propiedades.name}, ${((percentage).toFixed(2))}%`)
-//       }
-//   })
-//   console.log(arrayAssistance) 
-
-
-//   let minorAssistance= [...eventsPast]
-//   let arrayMinorAssistance= []
-//   minorAssistance.forEach(menor => {
-//       let percentage= (((menor.assistance100) / menor.capacity))
-//       if(percentage < 70) {
-//       return arrayMinorAssistance.push (`${propiedades.name}, ${((percentage).toFixed(2))}%`)
-//   }})
-//   console.log(arrayMinorAssistance) 
-
-
-//   let capacityTotal= [...listaHome]
-//   capacityTotal.sort((event1,event2)=>event2.capacity-event1.capacity)
-//   console.table(capacityTotal)
-
-//   containerHome.innerHTML = `
-//       <tr>
-//           <td class= "style: text-center"> ${arrayAssistance} </td> 
-//           <td class= "style: text-center"> ${arrayMinorAssistance} </td>
-//           <td class= "style: text-center"> ${capacityTotal[0].name},  ${capacityTotal[0].capacity }</td> 
-//       </tr>
+function renderTable1(data,events,container){
+  let finalAssistance=  filterAssistance (data) 
+  let finalMinerAssistance =filterMinerAssistance (data)
+  let finalCapacity= capacity (events)
   
-//   `
-// }
+  container.innerHTML = `
+      <tr>
+          <td class= "style: text-center"> ${finalAssistance} </td> 
+          <td class= "style: text-center"> ${finalMinerAssistance} </td>
+          <td class= "style: text-center"> ${finalCapacity[0].name}</td> 
+      </tr>
+  `
+
+}
+
+
 
 
 function renderTable2(data,container){
